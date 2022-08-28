934b83258d3b92de58f77234459d39641d4086d7
Vic <apulivic@gmail.com>
2022-08-28 15:20:59 +0800

    adding todo list

--- script/script.js
+++ script/script.js
@@ -2,6 +2,20 @@
 const dateEl = document.querySelector(".date");
 const bodyBG = document.querySelector("body");
 const greeting = document.querySelector(".greeting");
+const search = document.querySelector(".search-input");
+const searchLink = document.querySelector(".search-link");
+
+search.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    console.log(search.value);
+    searchLink.href = `https://www.google.com/search?q=${search.value}`;
+    searchLink.click();
+  }
+});
+
+// searchLink.addEventListener("click", () => {
+//   console.log("btn clicked");
+// });
 
 const months = [
   "January",
@@ -26,32 +40,33 @@ let minutes = date.getMinutes();
 let hours = date.getHours();
 let seconds = date.getSeconds();
 
-function refresh() {
+let random = Math.floor(Math.random() * 4);
+if (hours < 12) {
+  bodyBG.style.backgroundImage = `url(../assets/morning-${random}.jpg)`;
+  greeting.textContent = `Good morning, `;
+} else if (hours > 11 && hours < 18) {
+  bodyBG.style.backgroundImage = `url(../assets/afternoon-${random}.jpg)`;
+  greeting.textContent = `Good afternoon, `;
+} else {
+  bodyBG.style.backgroundImage = `url(../assets/night-${random}.jpg)`;
+  greeting.textContent = `Good evening, `;
+}
+
+function refreshTime() {
   const timeDisplay = document.querySelector(".time");
 
-  if (minutes < 10 && hours < 10) {
-    timeDisplay.textContent = `0${hours}:0${minutes}`;
-  } else if (hours < 10) {
-    timeDisplay.textContent = `0${hours}:${minutes}`;
-  } else if (minutes < 10) {
-    timeDisplay.textContent = `${hours}:0${minutes}`;
-  } else {
-    timeDisplay.textContent = `${hours}:${minutes}`;
-  }
-
-  if (day < 10) {
-    dateEl.textContent = `${month} / 0${day} / ${year}`;
-  } else {
-    dateEl.textContent = `${month} / ${day} / ${year}`;
-  }
-
-  // if (hours < 10) {
-  //   timeDisplay.textContent = `0${hours}:${minutes}`;
-  // } else if (hours < 10 && minutes < 10) {
-  //   timeDisplay.textContent = `0${hours}:0${minutes}`;
-  // }
+  timeDisplay.textContent = `${hours.toString().padStart(2, 0)}:${minutes
+    .toString()
+    .padStart(2, 0)}`;
+  dateEl.textContent = `${month} / ${day.toString().padStart(2, 0)} / ${year}`;
 }
-setInterval(refresh, 100);
+setInterval(refreshTime, 100);
+
+// if (hours < 10) {
+//   timeDisplay.textContent = `0${hours}:${minutes}`;
+// } else if (hours < 10 && minutes < 10) {
+//   timeDisplay.textContent = `0${hours}:0${minutes}`;
+// }
 
 // window.addEventListener("load", () => {
 //   for (let i = 0; i < 5; i++) {
@@ -65,20 +80,6 @@ setInterval(refresh, 100);
 //     }
 //   }
 // });
-
-for (let i = 0; i < 5; i++) {
-  let random = Math.floor(Math.random() * 4);
-  if (hours < 12) {
-    bodyBG.style.backgroundImage = `url(../assets/morning-${random}.jpg)`;
-    greeting.textContent = `Good morning, `;
-  } else if (hours > 12 && hours < 18) {
-    bodyBG.style.backgroundImage = `url(../assets/afternoon-${random}.jpg)`;
-    greeting.textContent = `Good afternoon, `;
-  } else {
-    bodyBG.style.backgroundImage = `url(../assets/night-${random}.jpg)`;
-    greeting.textContent = `Good evening, `;
-  }
-}
 
 // fetch("http://worldtimeapi.org/api/timezone/Asia/Manila")
 //   .then((res) => res.json())
