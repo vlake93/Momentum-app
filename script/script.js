c8ef2f27299d40f18d4a5c0e95a7b38a5b9dfc9f
Vic <apulivic@gmail.com>
2022-08-31 12:13:56 +0800

    removed button and put settings in one container

--- script/script.js
+++ script/script.js
@@ -1,12 +1,7 @@
 "use strict";
-const bodyBG = document.querySelector("body");
-const nameContainer = document.querySelector(".name-container");
-const container = document.querySelector(".container");
-// const name = document.querySelector(".name");
-const nameInput = document.querySelector(".name-input");
+
 const dateEl = document.querySelector(".date");
 const greeting = document.querySelector(".greeting");
-const greetingName = document.querySelector(".greeting-name");
 const search = document.querySelector(".search-input");
 const searchLink = document.querySelector(".search-link");
 const todoContainer = document.querySelector(".todo-container");
@@ -14,15 +9,14 @@ const todoBtn = document.querySelector(".todo-button");
 const addBtn = document.querySelector(".add-button");
 const todoList = document.querySelector(".todo-list-lister");
 const todoTask = document.querySelector(".todo-list-task");
-const focus = document.querySelector(".focus");
-const focusInput = document.querySelector(".focus-input");
-const focusHeader = document.querySelector(".focus-header");
-const yourFocus = document.querySelector(".your-focus");
 const quote = document.querySelector(".quotes-text");
 const quoteReset = document.querySelector(".quotes-button");
 
 // NAME AND FOCUS STORAGE
-let nameStorage = "";
+const nameContainer = document.querySelector(".name-container");
+const container = document.querySelector(".container");
+const nameInput = document.querySelector(".name-input");
+
 nameInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     let nameStorage = localStorage.setItem("name", nameInput.value);
@@ -43,6 +37,11 @@ if (localStorage.getItem("name") === null || undefined) {
   container.style.display = "flex";
 }
 
+const focus = document.querySelector(".focus");
+const focusInput = document.querySelector(".focus-input");
+const focusHeader = document.querySelector(".focus-header");
+const yourFocus = document.querySelector(".your-focus");
+
 focusInput.addEventListener("keydown", (e) => {
   let focusStorage = localStorage.setItem("focus", focusInput.value);
   if (e.key === "Enter") {
@@ -51,10 +50,7 @@ focusInput.addEventListener("keydown", (e) => {
       focus.style.display = "block";
     } else {
       focus.style.opacity = "0";
-      // focus.style.display = "absolute";
-      // focus.style.height = "1px";
       focusInput.style.opacity = "0";
-      // focusInput.style.width = "1px";
       focusHeader.style.opacity = "1";
       yourFocus.style.display = "block";
       yourFocus.textContent = `${localStorage.getItem("focus")}`;
@@ -108,27 +104,55 @@ let day = date.getDate();
 let hours = date.getHours();
 let random = Math.floor(Math.random() * 4);
 
+const bodyBG = document.querySelector("body");
+const greetingName = document.querySelector(".greeting-name");
+
 greetingName.textContent = localStorage.getItem("name");
 if (hours < 12) {
   bodyBG.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)), url(../assets/morning-${random}.jpg)`;
-  // greeting.textContent = `Good morning, ${localStorage.getItem("name")}`;
 } else if (hours > 11 && hours < 18) {
   bodyBG.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.15)), url(../assets/afternoon-${random}.jpg)`;
-  // greeting.textContent = `Good afternoon, ${localStorage.getItem("name")}`;
 } else {
   bodyBG.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.35)), url(../assets/night-${random}.jpg)`;
-  // greeting.innerHTML = `Good evening, ${localStorage.getItem("name")}`;
 }
+
+const circleLarge = document.querySelector(".circle-large");
+const time24 = document.querySelector(".time-24hr");
+const timeBtn = document.querySelector(".time-button");
+
+let ampm = false;
+timeBtn.addEventListener("click", () => {
+  !ampm ? ampm : !ampm;
+  if (!ampm) {
+    ampm = true;
+    circleLarge.classList.toggle("circle-off-large");
+  } else {
+    ampm = false;
+    circleLarge.classList.toggle("circle-off-large");
+  }
+});
 
 // TIME
 function refreshTime() {
+  const ticker = document.querySelector("#ampm");
   const timeDisplay = document.querySelector(".time");
   let minutes = new Date().getMinutes();
   let hours = new Date().getHours();
-  timeDisplay.textContent = `${hours.toString().padStart(2, 0)}:${minutes
-    .toString()
-    .padStart(2, 0)}`;
+  if (ampm === false) {
+    timeDisplay.textContent = `${hours.toString().padStart(2, 0)}:${minutes
+      .toString()
+      .padStart(2, 0)}`;
+  } else {
+    hours > 12
+      ? (timeDisplay.textContent = `${(hours % 12)
+          .toString()
+          .padStart(2, 0)}:${minutes.toString().padStart(2, 0)}pm`)
+      : (timeDisplay.textContent = `${(hours % 12)
+          .toString()
+          .padStart(2, 0)}:${minutes.toString().padStart(2, 0)}am`);
+  }
   dateEl.textContent = `${month} / ${day.toString().padStart(2, 0)} / ${year}`;
+
   greeting.textContent = `${localStorage.getItem("name")}`;
   if (hours < 12) {
     greeting.textContent = `Good morning, ${localStorage.getItem("name")}`;
@@ -140,70 +164,48 @@ function refreshTime() {
 }
 setInterval(refreshTime, 200);
 
-// QUOTES
+// SETTINGS
 const settingButton = document.querySelector(".setting-logo");
 const settingContainer = document.querySelector(".setting-container-hidden");
 const optionButtons = document.querySelectorAll(".setting-button");
 const optionTabs = document.querySelectorAll(".setting-options");
 
-// optionButtons.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     settingContainer.style.width = "40rem";
-//   });
-// });
-
 for (let i = 0; i < optionButtons.length; i++) {
   optionButtons[i].addEventListener("click", () => {
-    // if (optionTabs[i].classList.contains("hidden")) {
-    //   optionTabs[i].classList.remove("hidden");
-    // } else {
-    //   optionTabs[i].classList.add("hidden");
-    // }
-    settingContainer.classList.toggle("width-large");
-    // optionTabs[i].style.display = "flex";
     optionTabs[i].classList.toggle("hidden");
     optionButtons[i].classList.toggle("underline");
-    // e.target.classList.toggle("underline");
   });
 }
-
-// For each button, register an event listener
-// optionButtons.forEach((button) => {
-//   button.addEventListener("click", function (e) {
-//     // On click, remove the MyClass on ALL buttons
-//     optionTabs.forEach(function (el) {
-//       el.classList.toggle("hidden");
-//     });
-
-//     // Add the class on clicked one
-//     // e.target.classList.add("hidden");
-
-//     // Now pass the data-href to your iframe
-//     let theHREFtoOpen = e.target.getAttribute("data-href");
-//     console.log(theHREFtoOpen);
-//     //document.querySelector("#your-iframe").src = theHREFtoOpen
-//   });
-// });
 
 settingButton.addEventListener("click", () => {
   settingContainer.classList.toggle("hidden");
 });
 
+// QUOTES
 let quotesArr = [
   `"Swiper, no swiping, Swiper, no swiping!"`,
   `"Trying is the first step to failure"`,
 ];
 
-// ADD NEW QUOTE TO localstorage
-// push localstorage to array
-
 quote.textContent = quotesArr[0];
+localStorage.setItem("quote", quotesArr);
+// quote.textContent = localStorage.getItem("quote", quotesArr[0]);
+
+const addBtnQuote = document.querySelector(".add-button-quote");
+const addQuote = document.querySelector(".add-quote");
+
+addBtnQuote.addEventListener("click", addList);
+addQuote.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    addList();
+  }
+});
+
 quoteReset.addEventListener("click", () => {
   quote.textContent = quotesArr[Math.floor(Math.random() * quotesArr.length)];
 });
 
 // TODO
-
 todoBtn.addEventListener("click", () => {
   todoContainer.classList.toggle("hidden");
 });
@@ -239,46 +241,3 @@ circle.addEventListener("click", () => {
   circle.classList.toggle("circle-off");
   generalSearch.classList.toggle("general-off");
 });
-
-// addBtn.addEventListener("click", () => {
-//   for (let i = 0; i < 6; i++) {
-//     const newLi = document.createElement("li");
-//     localStorage.setItem("todo" + [i], todoTask.value);
-//     newLi.textContent = localStorage.getItem("todo" + [i]);
-//     todoList.appendChild(newLi);
-//     todoTask.value = "";
-//   }
-// });
-
-// if (hours < 10) {
-//   timeDisplay.textContent = `0${hours}:${minutes}`;
-// } else if (hours < 10 && minutes < 10) {
-//   timeDisplay.textContent = `0${hours}:0${minutes}`;
-// }
-
-// window.addEventListener("load", () => {
-//   for (let i = 0; i < 5; i++) {
-//     let random = Math.floor(Math.random() * 4);
-//     if (hours < 11) {
-//       bodyBG.style.backgroundImage = `url(../assets/morning-${random}.jpg)`;
-//     } else if (hours > 12 && hours < 17) {
-//       bodyBG.style.backgroundImage = `url(../assets/afternoon-${random}.jpg)`;
-//     } else {
-//       bodyBG.style.backgroundImage = `url(../assets/night-${random}.jpg)`;
-//     }
-//   }
-// });
-
-// fetch("http://worldtimeapi.org/api/timezone/Asia/Manila")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data);
-//     console.log(data.utc_datetime);
-//     timeEl.textContent = data.utc_datetime;
-//   });
-
-// fetch("https://api.pexels.com/v1/")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data);
-//   });
